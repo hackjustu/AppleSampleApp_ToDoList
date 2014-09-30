b81db8725d605f677503dec9c63d5831b1e8c96d
Citrixer <Citrixer@Cosmos-MacBook-Pro.local>
2014-09-30 16:26:12 -0700

    first commit

---
+++ ToDoList/XYZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XYZAppDelegate.h
+//  ToDoList
+//
+//  Created by Citrixer on 9/30/14.
+//  Copyright (c) 2014 ABC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
