4e72a6b19564b2afea00549d16139504fe3f254b
Citrixer <Citrixer@Cosmos-MacBook-Pro.local>
2014-09-30 21:32:58 -0700

    add Cancel and Done button as unwing segue

---
+++ XYZAddToDoItemViewController.h
@@ -0,0 +1,13 @@
+//
+//  XYZAddToDoItemViewController.h
+//  ToDoList
+//
+//  Created by Citrixer on 9/30/14.
+//  Copyright (c) 2014 ABC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZAddToDoItemViewController : UIViewController
+
+@end
---
+++ XYZToDoListTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  XYZToDoListTableViewController.h
+//  ToDoList
+//
+//  Created by Citrixer on 9/30/14.
+//  Copyright (c) 2014 ABC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZToDoListTableViewController : UITableViewController
+
+- (IBAction)unwindToList:(UIStoryboardSegue *)segue;
+
+@end
